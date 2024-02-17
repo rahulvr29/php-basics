3c3d192080f15b9749e39482d5671da43ab7dd37
RAHUL VR <iamrahulvr29@gmail.com>
2024-02-17 11:38:17 +0530

    sample code

--- sample.php
+++ sample.php
@@ -5,20 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>php Basics</title>
 </head>
-<body>
-
-<?php 
-
-
-
-
-
-
-?> 
-
-
-
-<h2>PHP Form Validation Example</h2>
+<body> 
+<!-- <h2>PHP Form Validation Example</h2>
 <p><span class="error">* Required field</span></p>
 <form method="post" action="">  
   Name: <input type="text" name="name" value="">
@@ -33,19 +21,41 @@
   Confirm password: <input type="password" name="confirm_password" value="">
   
   <br><br>
-  <!-- Gender:
+  Gender:
   <input type="radio" name="gender"  value="female">Female
   <input type="radio" name="gender" value="male">Male
   <input type="radio" name="gender"  value="other">Other  
   <span class="error"></span>
-  <br><br> -->
+  <br><br>
   <input type="submit" name="submit" value="Submit">  
-</form>
+</form> -->
 
 <?php 
 
 
+$color = ["red", "blue", "green", "yellow", "violet",];
 
+foreach ($color as $sample){
+  echo "$sample <br> " ;
+}
+echo "<br>";
+
+
+$fruits = array("apple", "Orange", "banana");
+
+foreach ($fruits as $sample){
+  echo "$sample <br> " ;
+}
+echo "<br>";
+
+$lesson = ["topic" => 1, "topic1" => 2, "topic2" => 3, "topic3"=> 4, "topic4" => 5,];
+
+foreach($lesson as $x => $y){
+  echo "$x : $y <br>" ;
+}
+
+?> 
+<?php
 
 
 
